adc380378216a302911a5dfdb2e0567948dfb273
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-05-30 20:41:38 +0530

    working on configuration

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,24 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import { FC } from "react";
+import { HomeIcon, LibraryIcon, SearchIcon } from "@heroicons/react/outline";
+const Sidebar: FC = () => {
+  return (
+    <div>
+      <div>
+        <button className='flex items-center'>
+          <HomeIcon className='h-5 w-5' />
+          <p>Home</p>
+        </button>
+        <button className='flex items-center'>
+          <SearchIcon className='h-5 w-5' />
+          <p>Search</p>
+        </button>
+        <button className='flex items-center'>
+          <LibraryIcon className='h-5 w-5' />
+          <p>Your Library</p>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
