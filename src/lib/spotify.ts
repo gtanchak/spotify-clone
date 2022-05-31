f590d7ef330238f0ca1ec2686ef18a23cabfeb88
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-05-31 23:08:46 +0530

    resolved env issue

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -17,3 +17,16 @@ const scopes = [
 ].join(",");
 
 const params = { scopes };
+
+const queryParamString = new URLSearchParams(params);
+
+const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
+
+export default spotifyApi;
+
+export { LOGIN_URL };
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,34 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { LOGIN_URL } from "../../../lib/spotify";
+
+const jwt = async ({ token, account, user }: any) => {
+  if (account && user) {
+    return {
+      ...token,
+      accessToken: account.access_token,
+      refreshToken: account.refresh_token,
+      username: account.providerAccountId,
+      accessTokenExpires: account.expires_at * 1000,
+    };
+  }
+};
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET!,
       authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET_TOKEN,
+  pages: {
+    signIn: "./login",
+  },
+  callbacks: {
+    jwt: jwt,
+  },
 });
