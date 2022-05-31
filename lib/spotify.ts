f46b31c10d0165aeeb1d332fe6bdb7d559652cdc
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-05-31 19:58:55 +0530

    working on spotify api integration

---
+++ lib/spotify.ts
@@ -0,0 +1,19 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = [
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "streaming",
+  "user-read-private",
+  "user-library-read",
+  "user-top-read",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-follow-read",
+  //"user-library-modify" risky
+].join(",");
+
+const params = { scopes };
